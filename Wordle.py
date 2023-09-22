a247f2f475746936f077e9cd42b9be78ab0ae32e
Matthias Hulme <mh828@byu.edu>
2023-09-22 13:47:44 -0600

    HardMode and Double Letter Bug Fix

--- Wordle.py
+++ Wordle.py
@@ -15,33 +15,43 @@ def wordle():
     ActualWord = ""
     ActualWord = random.choice(FIVE_LETTER_WORDS)
     print(ActualWord)
+
+    HardMode = True
+    GreenLetters = ['','','','','']
+    YellowLetters = []
     
     def enter_action(GuessWord):
         
         row = gw.get_current_row()
         GuessWord = GuessWord.lower()
-        if GuessWord not in FIVE_LETTER_WORDS:
-            gw.show_message("This is not a word")
-        else:
-            # set colors
 
+        counterMap = {}
+        for i in range(0,5):
+            if (ActualWord[i] in counterMap):
+                counterMap[ActualWord[i]] += 1
+            else:
+                counterMap[ActualWord[i]] = 1
+
+        if GuessWord not in FIVE_LETTER_WORDS:
+            gw.show_message("This is not a Word!")
+        elif (HardModeCheck(GuessWord) == False and HardMode != False):
+            gw.show_message("Must Include Previous Hints in Guess!")
+        else:
             for i in range(0,5):
-                print(GuessWord[i])
-                print(ActualWord[i])
                 if (GuessWord[i] == ActualWord[i]):
                     gw.set_square_color(row, i, CORRECT_COLOR)
-                elif (GuessWord[i] in ActualWord):
-                    gw.set_square_color(row, i, PRESENT_COLOR)
-                    
+                    GreenLetters[i] = GuessWord[i]
+                    counterMap[GuessWord[i]] -= 1
                 else:
                     gw.set_square_color(row, i, MISSING_COLOR)
-
-                
-
+            for i in range(0,5):
+                if (GuessWord[i] in ActualWord and counterMap[GuessWord[i]] > 0):
+                    gw.set_square_color(row, i, PRESENT_COLOR)
+                    YellowLetters.append(GuessWord[i])
+                    counterMap[GuessWord[i]] -= 1
     
-
             if GuessWord.lower() == ActualWord.lower():
-                gw.show_message("Correct you win")
+                gw.show_message("Correct you win!")
 
             elif row == 5:
                 gw.show_message("You lose. Correct word was " + ActualWord)
@@ -57,6 +67,15 @@ def wordle():
     gw = WordleGWindow()
 
     gw.add_enter_listener(enter_action)
+
+
+    def HardModeCheck(guess):
+        for letter in YellowLetters:
+            if letter not in guess:
+                return False
+        for i in range(0,5):
+            if GreenLetters[i] != guess[i] and GreenLetters[i] != '':
+                return False
 
 # Startup code
 
