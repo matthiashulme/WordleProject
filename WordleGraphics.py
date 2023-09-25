5db918dcd7d8803f1739f9c61fbb17a51892e876
staufjonbyu <staufjon@byu.edu>
2023-09-25 14:17:22 -0600

    backspace

--- WordleGraphics.py
+++ WordleGraphics.py
@@ -106,7 +106,7 @@ class WordleGWindow:
                 ch = tke.upper()
             else:
                 ch = tke.char.upper()
-            if ch == "\007" or ch == "\177" or ch == "DELETE":
+            if ch == "\b" or ch == "\177" or ch == "DELETE":
                 self.show_message("")
                 if self._row < N_ROWS and self._col > 0:
                     self._col -= 1
